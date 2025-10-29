406ea3db685c56610065c87f3bfc48341464fe70
Ali Rezai <ali.rezai27461@gmail.com>
2025-10-29 12:07:35 +0100

    created porjects

---
+++ apps/backend/blitzduel/src/main/java/se/examenarbete/blitzduel/BlitzduelApplication.java
@@ -0,0 +1,13 @@
+package se.examenarbete.blitzduel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlitzduelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlitzduelApplication.class, args);
+	}
+
+}
---
+++ apps/backend/blitzduel/src/test/java/se/examenarbete/blitzduel/BlitzduelApplicationTests.java
@@ -0,0 +1,13 @@
+package se.examenarbete.blitzduel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlitzduelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
