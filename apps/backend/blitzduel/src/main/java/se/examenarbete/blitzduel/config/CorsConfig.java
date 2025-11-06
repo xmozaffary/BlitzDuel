a5b4dd12f2a47744be7c3eb5d0f1a4595c1d234d
Rammsterr <94johram@gafe.molndal.se>
2025-11-06 12:28:18 +0100

    forgot to change url

--- apps/backend/blitzduel/src/main/java/se/examenarbete/blitzduel/config/CorsConfig.java
+++ apps/backend/blitzduel/src/main/java/se/examenarbete/blitzduel/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all endpoints
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "https://blitzduel.ittt.se")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
