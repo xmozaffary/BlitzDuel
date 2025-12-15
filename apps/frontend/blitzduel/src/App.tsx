95a95e0d3b0f7ba7bcecd952be338ac91dc107d7
Ali Rezai <ali.rezai27461@gmail.com>
2025-12-15 13:28:02 +0100

    fixed sound for mobile

--- apps/frontend/blitzduel/src/App.tsx
+++ apps/frontend/blitzduel/src/App.tsx
@@ -2,13 +2,15 @@ import { RouterProvider } from "react-router-dom";
 import "./App.scss";
 import { router } from "./Router";
 import { PlayerProvider } from "./contexts/PlayerContext";
-// CI/CD test
-// CI/CD test
+import { unlockAudio } from "./utils/playSound";
+
 function App() {
   return (
-    <PlayerProvider>
-      <RouterProvider router={router} />
-    </PlayerProvider>
+    <div onClick={unlockAudio} onTouchStart={unlockAudio}>
+      <PlayerProvider>
+        <RouterProvider router={router} />
+      </PlayerProvider>
+    </div>
   );
 }
 
--- apps/frontend/blitzduel/src/utils/playSound.ts
+++ apps/frontend/blitzduel/src/utils/playSound.ts
@@ -1,3 +1,20 @@
+let audioUnlocked = false;
+
+export const unlockAudio = () => {
+  if (audioUnlocked) return;
+
+  const audio = new Audio();
+  audio
+    .play()
+    .then(() => {
+      audio.pause();
+      audioUnlocked = true;
+    })
+    .catch(() => {
+      // Ignore error
+    });
+};
+
 export const playSound = (soundName: string) => {
   const audio = new Audio(`/sounds/${soundName}`);
   audio.play().catch((error) => {
